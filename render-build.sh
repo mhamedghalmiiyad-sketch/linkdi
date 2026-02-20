374716ec2846e8e9565c4bd214e74836fb0152a0
mhamedghalmiiyad-sketch <mhamed.ghalmi.iyad@gmail.com>
2026-02-20 19:05:48 +0100

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+# Force Chrome to install inside the project folder so Render doesn't delete it
+export PUPPETEER_CACHE_DIR=/opt/render/project/src/.cache
 npm install
 npx puppeteer browsers install chrome
